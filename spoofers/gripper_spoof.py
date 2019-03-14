ce1857bc883a20442c0db9112166e30b5e091aa5
Daniel Gosden <daniel.gosden@gmail.com>
2019-03-14 10:54:02 +0000

    Added spoofers for the gripper, motion executor, and vacuum control - but they're untested currently.

--- spoofers/gripper_spoof.py
+++ spoofers/gripper_spoof.py
@@ -1,1 +1,41 @@
+#!/usr/bin/env python
 
+import rospy
+from std_msgs.msg import String,Bool,Int8
+
+def gripper_c_loop_callback(data):
+    rospy.loginfo("Gripper received data from finger pos")
+    rospy.loginfo(data)
+
+def spoofer():
+    rospy.init_node('gripper', anonymous=True)
+    grip_sensor_pub = rospy.Publisher('grip_sensor', Int8, queue_size=10)
+
+    rospy.Subscriber('finger_pos', Bool, gripper_c_loop_callback)
+
+    rate = rospy.Rate(0.5)
+
+    state = 0
+
+    while not rospy.is_shutdown():
+        if (state==0):
+          grip_sensor_pub.publish(55)
+        elif (state == 1):
+          grip_sensor_pub.publish(155)
+        elif (state == 2):
+          grip_sensor_pub.publish(255)
+        elif (state ==3):
+          grip_sensor_pub.publish(100)
+        
+        state = state +1
+        if state >= 4:
+          state = 0
+        rospy.loginfo("Grip sensor state output to grip_sensor")
+ 
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        spoofer()
+    except rospy.ROSInterruptException:
+        pass
--- spoofers/vacuum_spoof.py
+++ spoofers/vacuum_spoof.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
 
+import rospy
+from std_msgs.msg import String,Bool,Int8
+from geometry_msgs.msg import Transform,Vector3,Quaternion
+from scooper_duper.msg import *
+
+def vacuum_c_loop_callback(data):
+    rospy.loginfo("Vacuum control received data from suction_state")
+    rospy.loginfo(data)
+
+
+def spoofer():
+    rospy.init_node('vacuum', anonymous=True)
+
+    rospy.Subscriber('suction_state', Bool, vacuum_c_loop_callback)
+    rate = rospy.Rate(0.5)
+        
+    rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        spoofer()
+    except rospy.ROSInterruptException:
+        pass
