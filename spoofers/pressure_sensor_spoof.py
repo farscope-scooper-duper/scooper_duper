970473ad189567f9a31e6a00744647443eea1c3e
jhird11 <j.hird@bristol.ac.uk>
2019-04-30 11:00:03 +0100

    Added pressure sensor spoofer

---
+++ spoofers/pressure_sensor_spoof.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import signal
+import sys
+from std_msgs.msg import Float64
+
+def spoofer():
+    rospy.init_node('pressure_sensor_spoof', anonymous=True)
+    pressure_sensor_pub = rospy.Publisher('pressure_sensor', Float64, queue_size=10)
+
+    rate = rospy.Rate(1.1)
+    reading = 1000.54
+    while not rospy.is_shutdown():
+        pressure_sensor_pub.publish(reading)
+        rospy.time
+        rospy.loginfo("Pressure reading: {}".format(reading))
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        spoofer()
+    except rospy.ROSInterruptException:
+        pass
